a322329f74890c522e1144f6b5df3e6889b5080e
JasOnRadC1iFfe <jason421202795@gmail.com>
2015-06-19 23:11:07 +0800

    ignore file from website

--- 3DCG_Hw4/Debug/RUN_SAMPLE.bat
+++ 3DCG_Hw4/Debug/RUN_SAMPLE.bat
@@ -1,15 +1,1 @@
-# run sample
-
-HelloGlut.exe tilt0.in
-
-HelloGlut.exe tilt1.in
-
-HelloGlut.exe tilt2.in
-
-HelloGlut.exe tilt3.in
-
-HelloGlut.exe tilt4.in
-
-HelloGlut.exe tilt5.in
-
-HelloGlut.exe chair.in
+HelloGlut.exe Hw4F.in
